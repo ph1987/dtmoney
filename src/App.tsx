5117a38500d8a390b50e9888997acc8d09a8fe7c
Philipe <philipe.tavares@skyone.solutions>
2023-03-15 20:37:40 -0300

    frontend 100% finished. using static data w/ mirageJS

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
+import Modal from 'react-modal';
 import { Header } from "./components/Header";
+import { Dashboard } from "./components/Dashboard";
 import { GlobalStyle } from "./styles/global";
+import { useState } from 'react';
+import { TransactionsProvider } from './hooks/useTransactions';
+import { NewTransactionModal } from "./components/NewTransactionModal";
+
+Modal.setAppElement('#root');
 
 export function App() {
-  return (
-    <>
-      <Header />
-      <GlobalStyle />
-    </>
-  );
+
+    const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+    function handleOpenNewTransactionModal() {
+        setIsNewTransactionModalOpen(true);
+    }
+
+    function handleCloseNewTransactionModal() {
+        setIsNewTransactionModalOpen(false);
+    }
+
+    return (
+        <TransactionsProvider>
+            <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+            <Dashboard />
+            <NewTransactionModal 
+                isOpen={isNewTransactionModalOpen} 
+                onRequestClose={handleCloseNewTransactionModal} 
+            />
+            <GlobalStyle />
+        </TransactionsProvider>
+    );
 }
